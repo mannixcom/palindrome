afffbe1a5bc758db26c5b15d6bde5ed322d4418d
Mannix <mannixcom@gmail.com>
2021-10-08 15:07:10 +0100

    Finish working and refactoring palindrome method

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
-// Reverses a string
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+ module.exports = Phrase;
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-// Defines a Phrasse object.
+// Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function(string) {
-    return  this.string.toLowerCase();
-  }
-
-  // Returns content processed for palindrome testing
+  // Returns content processed for palindrome testing, by setting everything all letters to lower case.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.letters().toLowerCase();
   }
 
-  //Returns true for a palindrome. False otherwise.
+  // Returns the letters in the content.
+  // For example:
+  //  new Phrase("hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+   let theLetters = [];
+   return (this.content.match(/[a-z]/gi) || []).join("");
+ }
+
+
+  // Returns the letters in the content.
+  //this.letters = function letters() {
+    //return this.content;    // stub return value
+//  }
+
+
+  // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === reverse(this.processedContent());
-  }
-
-  // Makes the Phrase Louder!!
-  this.louder = function louder() {
-    let loud = this.content.toUpperCase();
-    return loud
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
-
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  //Returns translation processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.processor(this.translation) ;
-  }
-}
-TranslatedPhrase.prototype = new Phrase();
